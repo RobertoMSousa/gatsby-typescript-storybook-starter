607a1bbdff66b5535d1871a3ea2be86c606f7a82
Roberto Sousa <beto.sousa22@gmail.com>
2019-11-03 15:55:09 +0000

    import declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '@storybook/react/demo';
